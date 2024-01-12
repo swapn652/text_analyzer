16c22c510b426056b28994a037c6f8ac04043dde
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2024-01-12 16:48:22 +0530

    Added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Intro } from "./components/Intro"
+import { MainSection } from "./components/MainSection"
 
 function App() {
-
   return (
-    <>
-    
-    </>
+    <div className = "p-[1rem] md:p-[2rem] min-h-screen" style = {{background: "radial-gradient(100% 100% at 49.12% 0%, #FFFFFF 0%, #EFF6FF 100%)"}}>
+      <Intro/>
+      <MainSection/>
+    </div>
   )
 }
 
